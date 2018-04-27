e1f5951c651e2ca30fbfdc4f54c60c694b1ffbf6
Tiago Moretto <moretto.tiago@gmail.com>
2018-04-26 22:46:06 -0300

    Criando serviço de usuário

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { ListuserComponent } from './components/listuser/listuser.component';
+import { UserService } from './shared-service/user.service';
 
 const appRoutes:Routes=[
   { path: '', component:ListuserComponent }
@@ -16,6 +18,7 @@ const appRoutes:Routes=[
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     RouterModule.forRoot(appRoutes)
   ],
   providers: [],
